3588db50599f9d366b0bbfebfc5f0cbddf70512f
Aubrey Kodar <Hlungwaneak@protonmail.com>
2019-03-28 17:38:32 +0000

    Updates Config Not Found Exception

--- src/Exceptions/ConfigNotFoundException.php
+++ src/Exceptions/ConfigNotFoundException.php
@@ -4,7 +4,8 @@ namespace AubreyKodar\Payfast\Exceptions;
 
 class ConfigNotFoundException extends \Exception
 {
-    public function __construct($message, $code = 0, Exception $previous = null)
+    public function __construct(Exception $previous = null)
     {
+        parent::__construct("Merchant configurations not found", 404, $previous);
     }
 }
