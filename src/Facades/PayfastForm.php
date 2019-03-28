a9aa6599ac3b4edbf2c208b8a74824a956253c13
Aubrey Kodar <Hlungwaneak@protonmail.com>
2019-03-28 11:49:32 +0000

    Added facades

---
+++ src/Facades/PayfastForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AubreyKodar\Payfast\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PayfastForm extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     */
+    protected static function getFacadeAccessor()
+    {
+       return 'aubreykodar.payfast.form';
+    }
+}
