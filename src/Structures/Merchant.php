55c2af85da365d685e0c1f98906dd4072f56c5eb
Aubrey Kodar <Hlungwaneak@protonmail.com>
2019-03-28 17:37:56 +0000

    Updates merchant and routes structures

--- src/Structures/Merchant.php
+++ src/Structures/Merchant.php
@@ -1,9 +1,27 @@
 <?php
 
-
 namespace AubreyKodar\Payfast\Structures;
 
 class Merchant
 {
+    /**
+     * @var string Merchant Id
+     */
+    public $merchant_id;
 
+    /**
+     * @var string Merchant Key
+     */
+    public $merchant_key;
+
+    /**
+     * Merchant constructor.
+     * @param string $merchant_id
+     * @param string $merchant_key
+     */
+    public function __construct(string $merchant_id, string $merchant_key)
+    {
+        $this->merchant_id = $merchant_id;
+        $this->merchant_key = $merchant_key;
+    }
 }
--- src/Structures/Routes.php
+++ src/Structures/Routes.php
@@ -1,9 +1,40 @@
 <?php
 
-
 namespace AubreyKodar\Payfast\Structures;
 
 class Routes
 {
+    /**
+     * Success URL
+     *
+     * @var string
+     */
+    public $return_url;
 
+    /**
+     * Cancel URL
+     *
+     * @var string
+     */
+    public $cancel_url;
+
+    /**
+     * Notify URL
+     *
+     * @var string
+     */
+    public $notify_url;
+
+    /**
+     * Routes constructor.
+     * @param string $notify_url
+     * @param string $return_url
+     * @param string $cancel_url
+     */
+    public function __construct(string $notify_url, string  $return_url, string $cancel_url)
+    {
+        $this->notify_url = $notify_url;
+        $this->return_url = $return_url;
+        $this->cancel_url = $cancel_url;
+    }
 }
