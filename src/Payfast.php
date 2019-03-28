7cd6e0abeb3194611a38b303f8cbf28f6c14d695
Aubrey Kodar <Hlungwaneak@protonmail.com>
2019-03-28 13:05:33 +0000

    Added dockblocks

--- src/Payfast.php
+++ src/Payfast.php
@@ -10,28 +10,43 @@ use AubreyKodar\Payfast\Structures\Routes;
 class Payfast
 {
     /**
-     * Payfast constructor.
-     *     'merchant_id'=>'',
-    'merchant_key'=>'',
-    'env'=>'',
-    'return_url'=>'',
-    'cancel_url'=>'',
-    'notify_url'=>''
+     * @var Merchant
      */
-
     protected $merchant;
+
+    /**
+     * @var Routes
+     */
     protected $routes;
+
+    /**
+     * @var string
+     */
     protected $env;
 
+    /**
+     * Payfast constructor.
+     * @param string $env
+     * @param Merchant|null $merchant
+     * @param Routes|null $routes
+     */
     public function __construct(string $env = Environment::PRODUCTION, Merchant $merchant = null, Routes $routes = null)
     {
         $this->env = $env;
         $this->merchant = null === $merchant? $this->getMerchant(): $merchant;
         $this->routes = null === $routes? $this->getRoutes(): $routes;
     }
+
+    /**
+     * @return Merchant
+     */
     private function getMerchant(): Merchant
     {
     }
+
+    /**
+     * @return Routes
+     */
     private function getRoutes(): Routes
     {
     }
